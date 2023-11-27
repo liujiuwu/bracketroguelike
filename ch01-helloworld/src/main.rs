300becc26e1c14ebb28a3049b19b98be7bab5875
jiuwu <923933533@qq.com>
2023-11-27 10:35:45 +0800

    refactor

--- ch01-helloworld/src/main.rs
+++ ch01-helloworld/src/main.rs
@@ -1,16 +1,17 @@
 use bracket_lib::prelude::*;
 
 struct State {}
+
 impl GameState for State {
-    fn tick(&mut self, ctx : &mut BTerm) {
+    fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Bracket");
+        ctx.print_centered(25, "Hello world!");
     }
 }
 
 fn main() -> BError {
     let ctx = BTermBuilder::simple80x50().build()?;
 
-    let gs = State{ };
+    let gs = State {};
     main_loop(ctx, gs)
 }

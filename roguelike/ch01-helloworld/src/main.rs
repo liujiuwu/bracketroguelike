2d1072acec531a0ca05daf7cb07c9059128ca8f1
jiuwu <923933533@qq.com>
2023-12-04 16:09:55 +0800

    remove helloecs-life

--- roguelike/ch01-helloworld/src/main.rs
+++ roguelike/ch01-helloworld/src/main.rs
@@ -5,12 +5,17 @@ struct State {}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print_centered(25, "Hello world!");
+        for i in 1..=5 {
+            ctx.draw_box(25 + i, 15 + i, 30 - i * 2, 20 - i * 2, WHITE, BLACK);
+        }
+        ctx.print_color_centered(25, YELLOW, BLACK, "Hello world!");
     }
 }
 
 fn main() -> BError {
-    let ctx = BTermBuilder::simple80x50().build()?;
+    let ctx = BTermBuilder::simple80x50()
+        .with_title("Roguelike game")
+        .build()?;
 
     let gs = State {};
     main_loop(ctx, gs)
--- roguelike/ch02-helloecs-life/src/universe.rs
+++
@@ -1,7 +0,0 @@
-use ch02_helloecs_life::cell::Cell;
-
-struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
-}
